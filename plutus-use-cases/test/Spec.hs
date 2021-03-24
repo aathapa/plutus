08923cddd1d67883c069e593e8fad4e473547c6c
silky <noonsilk+-noonsilk@gmail.com>
2021-03-24 17:43:02 +0000

    simple escrow contract and tests

--- plutus-use-cases/test/Spec.hs
+++ plutus-use-cases/test/Spec.hs
@@ -8,6 +8,7 @@ import qualified Spec.ErrorHandling
 import qualified Spec.Escrow
 import qualified Spec.Future
 import qualified Spec.GameStateMachine
+import qualified Spec.SimpleEscrow
 -- import qualified Spec.MultiSig
 import qualified Spec.MultiSigStateMachine
 import qualified Spec.PingPong
@@ -43,6 +44,7 @@ tests = localOption limit $ testGroup "use cases" [
     Spec.Currency.tests,
     Spec.PubKey.tests,
     Spec.Escrow.tests,
+    Spec.SimpleEscrow.tests,
     Spec.GameStateMachine.tests,
     Spec.Rollup.tests,
     Spec.TokenAccount.tests,
